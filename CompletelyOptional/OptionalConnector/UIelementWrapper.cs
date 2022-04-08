b4141bec33efd2fd033de45025a10ee3773f1298
topicular <40355580+metnias@users.noreply.github.com>
2022-04-08 19:38:40 +0900

    Renamed OptionInterface folder to OptionalConnector to avoid confusion with OptionInterface.cs

---
+++ CompletelyOptional/OptionalConnector/UIelementWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Menu;
+using UnityEngine;
+using OptionalUI;
+
+namespace CompletelyOptional
+{
+    public class UIelementWrapper : PositionedMenuObject
+    {
+        public UIelementWrapper(Menu.Menu menu, MenuObject owner, Vector2 pos, UIelement element) : base(menu, owner, pos)
+        {
+        }
+    }
+}
